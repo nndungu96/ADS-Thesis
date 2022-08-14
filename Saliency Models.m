c259d04642c15f058f4cac14b317239601772b38
nndungu96 <91328388+nndungu96@users.noreply.github.com>
2022-08-14 12:47:37 +0100

    Add files via upload

---
+++ Saliency Models.m
@@ -0,0 +1,51 @@
+img_path = 'C:\Users\njeri\Documents\Masters\Thesis\Scripts\Data\ads_data\data\image_hd.png'
+Aim_sal = AIM_wrap(img_path)
+imwrite(Aim_sal, ['C:\Users\njeri\Documents\Masters\Thesis\Scripts\Data\Maps\aim_sal.png'])
+
+QSS_map = QSS_wrap(img_path);
+imwrite(QSS_map, ['C:\Users\njeri\Documents\Masters\Thesis\Scripts\Data\Maps\qss_sal.png'])
+
+AWS_map = AWS_wrap(img_path);
+imwrite(AWS_map, ['C:\Users\njeri\Documents\Masters\Thesis\Scripts\Data\Maps\aws_sal.png'])
+
+IKN_map = IKN_wrap(img_path);
+imwrite(IKN_map, ['C:\Users\njeri\Documents\Masters\Thesis\Scripts\Data\Maps\ikn_sal.png'])
+
+CAS_map = CAS_wrap(img_path);
+imwrite(CAS_map, ['C:\Users\njeri\Documents\Masters\Thesis\Scripts\Data\Maps\cas_sal.png'])
+
+SUN_map = SUN_wrap(img_path);
+imwrite(SUN_map, ['C:\Users\njeri\Documents\Masters\Thesis\Scripts\Data\Maps\sun_sal.png'])
+
+cvs_map = CVS_wrap(img_path);
+imwrite(cvs_map, ['C:\Users\njeri\Documents\Masters\Thesis\Scripts\Data\Maps\cvs_sal.png'])
+
+dva_map = DVA_wrap(img_path);
+imwrite(dva_map, ['C:\Users\njeri\Documents\Masters\Thesis\Scripts\Data\Maps\dva_sal.png'])
+
+fes_map = FES_wrap(img_path);
+imwrite(fes_map, ['C:\Users\njeri\Documents\Masters\Thesis\Scripts\Data\Maps\fes_sal.png'])
+
+gbvs_map = GBVS_wrap(img_path);
+imwrite(gbvs_map, ['C:\Users\njeri\Documents\Masters\Thesis\Scripts\Data\Maps\gbvs_sal.png'])
+
+imsig_map = IMSIG_wrap(img_path);
+imwrite(imsig_map, ['C:\Users\njeri\Documents\Masters\Thesis\Scripts\Data\Maps\imsig_sal.png'])
+
+lds_map = LDS_wrap(img_path);
+imwrite(lds_map, ['C:\Users\njeri\Documents\Masters\Thesis\Scripts\Data\Maps\lds_sal.png'])
+
+rare_map = RARE2012_wrap(img_path);
+imwrite(rare_map, ['C:\Users\njeri\Documents\Masters\Thesis\Scripts\Data\Maps\rare_sal.png'])
+
+ssr_map = SSR_wrap(img_path);
+imwrite(ssr_map, ['C:\Users\njeri\Documents\Masters\Thesis\Scripts\Data\Maps\ssr_sal.png'])
+
+gauss_map = gaussian_wrap(img_path);
+imwrite(gauss_map, ['C:\Users\njeri\Documents\Masters\Thesis\Scripts\Data\Maps\gauss_sal.png'])
+
+
+
+
+
+
